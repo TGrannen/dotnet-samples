601b98ccf3fafe3a1234e806782de6d7ab23f3df
Tim Grannen <grannendev@gmail.com>
2021-08-08 10:29:42 -0400

    Update mediator fixture to better pattern

--- Testing/Mocking/Mocking.Mediator.Tests/Fixtures/ApplicationFixture.cs
+++ Testing/Mocking/Mocking.Mediator.Tests/Fixtures/ApplicationFixture.cs
@@ -1,12 +1,13 @@
-using Microsoft.Extensions.DependencyInjection;
-
 namespace Mocking.Mediator.Tests.Fixtures
 {
     public class ApplicationFixture : MediatorFixture
     {
-        public override void AddBindings(IServiceCollection services)
+        public ApplicationFixture()
         {
-            services.AddApplication();
+            OnConfigureServices += (_, services) =>
+            {
+                services.AddApplication();
+            };
         }
     }
 }
--- Testing/Mocking/Mocking.Mediator.Tests/Fixtures/MediatorFixture.cs
+++ Testing/Mocking/Mocking.Mediator.Tests/Fixtures/MediatorFixture.cs
@@ -1,3 +1,4 @@
+using System;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using Mocking.Mediator.Tests.Exceptions;
@@ -9,23 +10,30 @@ namespace Mocking.Mediator.Tests.Fixtures
 {
     public class MediatorFixture
     {
-        public ServiceProvider Provider { get; private set; }
-
+        private ServiceProvider _provider;
         protected ServiceCollection Services { get; } = new ServiceCollection();
+        public EventHandler<IServiceCollection> OnConfigureServices { get; set; }
 
-        public virtual void AddBindings(IServiceCollection services)
+        public ServiceProvider Provider
         {
+            get
+            {
+                if (_provider != null)
+                {
+                    return _provider;
+                }
+
+                Services.AddMockedLogging();
+                OnConfigureServices?.Invoke(this, Services);
+                Provider = Services.BuildServiceProvider();
+
+                return _provider;
+            }
+            private set => _provider = value;
         }
 
         public async Task<T> SendAsync<T>(IRequest<T> request)
         {
-            if (Provider == null)
-            {
-                Services.AddMockedLogging();
-                AddBindings(Services);
-                Provider = Services.BuildServiceProvider();
-            }
-
             var mediator = Provider.GetService<IMediator>();
 
             if (mediator == null)
@@ -35,6 +43,20 @@ namespace Mocking.Mediator.Tests.Fixtures
 
             return await mediator.Send(request);
         }
+
+
+        public async Task Publish(INotification notification)
+        {
+            var mediator = Provider.GetService<IMediator>();
+
+            if (mediator == null)
+            {
+                throw new MediatorFixtureConfigurationException();
+            }
+
+            await mediator.Publish(notification);
+        }
+
 
         public MockedILogger<T> GetMockedLogger<T>()
         {
