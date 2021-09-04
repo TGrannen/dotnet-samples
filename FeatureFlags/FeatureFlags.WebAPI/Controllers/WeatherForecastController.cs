577345f8a1f36a75eebda29a39107164dde35bd5
Tim Grannen <grannendev@gmail.com>
2021-09-04 10:37:44 -0400

    Add Basic Feature Flag project and example

---
+++ FeatureFlags/FeatureFlags.WebAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.FeatureManagement;
+
+namespace FeatureFlags.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IFeatureManager _featureManager;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IFeatureManager featureManager)
+        {
+            _logger = logger;
+            _featureManager = featureManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            if (!await _featureManager.IsEnabledAsync("ShowWeather"))
+            {
+                return Unauthorized();
+            }
+
+            var rng = new Random();
+            var weatherForecasts =
+                Enumerable.Range(1, 5)
+                    .Select(index => new WeatherForecast
+                    {
+                        Date = DateTime.Now.AddDays(index),
+                        TemperatureC = rng.Next(-20, 55),
+                        Summary = Summaries[rng.Next(Summaries.Length)]
+                    })
+                    .ToArray();
+            return Ok(weatherForecasts);
+        }
+    }
+}
