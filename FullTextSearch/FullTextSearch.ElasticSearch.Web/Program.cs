dced9f95f2d31118cc7293f527598e08c8e526cb
Tim Grannen <grannendev@gmail.com>
2022-08-15 01:08:33 -0400

    Add the ability to create Elastic Search containers

--- FullTextSearch/FullTextSearch.ElasticSearch.Web/Program.cs
+++ FullTextSearch/FullTextSearch.ElasticSearch.Web/Program.cs
@@ -1,4 +1,8 @@
 using System.Net;
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Configurations;
+using DotNet.Testcontainers.Containers;
+using DotNet.Testcontainers.Networks;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -26,4 +30,43 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+var networkBuilder = new TestcontainersNetworkBuilder()
+    .WithName("elastic")
+    .WithDriver(NetworkDriver.Bridge);
+
+var network = networkBuilder.Build();
+try
+{
+    await network.CreateAsync();
+    var name = "elasticsearch-dotnet-samples";
+    var containersBuilder = new TestcontainersBuilder<TestcontainersContainer>()
+        .WithImage("elasticsearch:8.3.3")
+        .WithName(name)
+        .WithEnvironment("xpack.security.enabled", "false")
+        .WithEnvironment("discovery.type", "single-node")
+        .WithPortBinding(9200)
+        .WithPortBinding(9300)
+        .WithNetwork(network);
+
+    var containersBuilder2 = new TestcontainersBuilder<TestcontainersContainer>()
+        .WithImage("kibana:8.3.3")
+        .WithName("kibana-dotnet-samples")
+        // .WithEnvironment("ELASTICSEARCH_HOSTS", $"http://{name}:9200")
+        .WithPortBinding(5601)
+        .WithNetwork(network);
+
+    await using var containers = containersBuilder.Build();
+    await using var containers2 = containersBuilder2.Build();
+    await containers.StartAsync();
+    await containers2.StartAsync();
+
+    app.Run();
+}
+catch (Exception e)
+{
+    Log.Error(e, "ERROR");
+}
+finally
+{
+    await network.DeleteAsync();
+}
