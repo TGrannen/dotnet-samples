561b418120086a6b9d35c70be01c5b78517a59e5
Tim Grannen <grannendev@gmail.com>
2022-08-20 16:02:58 -0400

    Move container setup to separate service

--- FullTextSearch/FullTextSearch.ElasticSearch.Web/Program.cs
+++ FullTextSearch/FullTextSearch.ElasticSearch.Web/Program.cs
@@ -1,6 +1,3 @@
-using DotNet.Testcontainers.Builders;
-using DotNet.Testcontainers.Configurations;
-using DotNet.Testcontainers.Containers;
 using FullTextSearch.ElasticSearch.Web.Services;
 using Nest;
 using Serilog;
@@ -22,7 +19,8 @@ settings.EnableApiVersioningHeader();
 var client = new ElasticClient(settings);
 
 builder.Services.AddSingleton(client);
-builder.Services.AddSingleton<Seed>();
+builder.Services.AddSingleton<ContainerService>();
+builder.Services.AddSingleton<SeedService>();
 
 var app = builder.Build();
 
@@ -40,43 +38,14 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-var networkBuilder = new TestcontainersNetworkBuilder()
-    .WithName("elastic")
-    .WithDriver(NetworkDriver.Bridge);
+var containerService = app.Services.GetRequiredService<ContainerService>();
+var seeder = app.Services.GetRequiredService<SeedService>();
 
-var network = networkBuilder.Build();
 try
 {
-    Log.Information("Creating Network");
-    await network.CreateAsync();
-
-    var name = "elasticsearch-dotnet-samples";
-    var containersBuilder = new TestcontainersBuilder<TestcontainersContainer>()
-        .WithImage("elasticsearch:8.3.3")
-        .WithName(name)
-        .WithEnvironment("xpack.security.enabled", "false")
-        .WithEnvironment("discovery.type", "single-node")
-        .WithPortBinding(9200)
-        .WithPortBinding(9300)
-        .WithNetwork(network)
-        .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(9300));
-
-    var containersBuilder2 = new TestcontainersBuilder<TestcontainersContainer>()
-        .WithImage("kibana:8.3.3")
-        .WithName("kibana-dotnet-samples")
-        .WithEnvironment("ELASTICSEARCH_HOSTS", $"http://{name}:9200")
-        .WithPortBinding(5601)
-        .WithNetwork(network);
-
-    Log.Information("Building Containers");
-    await using var containers = containersBuilder.Build();
-    await using var containers2 = containersBuilder2.Build();
-    Log.Information("Starting Containers");
-    await containers.StartAsync();
-    await containers2.StartAsync();
-
-    var seeder = app.Services.GetRequiredService<Seed>();
-    await seeder.SeedData(index);
+    await containerService.RunContainers();
+    await seeder.CreateIndex(index);
+    await seeder.SeedData(10);
 
     app.Run();
 }
@@ -86,5 +55,5 @@ catch (Exception e)
 }
 finally
 {
-    await network.DeleteAsync();
+    await containerService.StopContainers();
 }
---
+++ FullTextSearch/FullTextSearch.ElasticSearch.Web/Services/SeedService.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using Bogus;
+using Nest;
+
+namespace FullTextSearch.ElasticSearch.Web.Services;
+
+public class SeedService
+{
+    private readonly ElasticClient _client;
+    private readonly ILogger<SeedService> _logger;
+    private readonly Faker<LogDocument> _logsFaker;
+
+    public SeedService(ElasticClient client, ILogger<SeedService> logger)
+    {
+        Randomizer.Seed = new Random(123456);
+        _client = client;
+        _logger = logger;
+        _logsFaker = new Faker<LogDocument>()
+            .RuleFor(o => o.Id, Guid.NewGuid)
+            .RuleFor(o => o.Body, f => f.Random.Words());
+    }
+
+    public async Task SeedData(int count)
+    {
+        var timer = new Stopwatch();
+        timer.Start();
+
+        _logger.LogInformation("Writing Documents");
+        var documents = _logsFaker.Generate(count);
+        foreach (var document in documents)
+        {
+            await _client.CreateDocumentAsync(new LogDocument()
+            {
+                Body = document.Body.Trim()
+            });
+        }
+
+        timer.Stop();
+        _logger.LogInformation("Documents created in {Elapsed}ms with {Count} element", timer.Elapsed, documents.Count);
+    }
+
+    public async Task CreateIndex(string indexName)
+    {
+        _logger.LogInformation("Creating Index");
+        var timer = new Stopwatch();
+        timer.Start();
+
+        var createIndexResponse = await _client.Indices.CreateAsync(indexName, c => c.Map<LogDocument>(m => m.AutoMap<LogDocument>()));
+        _logger.LogInformation("Index Created in {Elapsed}ms. Response: {@Response}", timer.Elapsed, createIndexResponse);
+        timer.Stop();
+    }
+}
