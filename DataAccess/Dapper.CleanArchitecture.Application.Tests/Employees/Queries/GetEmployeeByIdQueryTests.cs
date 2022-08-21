92e52ed188597e239f55daf344763c0cb396b0c1
Tim Grannen <grannendev@gmail.com>
2022-08-21 00:40:37 -0400

    Add example query test

---
+++ DataAccess/Dapper.CleanArchitecture.Application.Tests/Employees/Queries/GetEmployeeByIdQueryTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Dapper.CleanArchitecture.Application.Employees.Queries;
+using Dapper.CleanArchitecture.Domain.Employees;
+
+namespace Dapper.CleanArchitecture.Application.Tests.Employees.Queries;
+
+public class GetEmployeeByIdQueryTests
+{
+    private readonly ApplicationFixture _fixture = new();
+    private const int EmployeeNumber = 451;
+
+    public GetEmployeeByIdQueryTests()
+    {
+        _fixture.DbConnection
+            .SetupDapperAsync(c => c.QueryFirstOrDefaultAsync<Employee>(It.IsAny<string>(), null, null, null, null))
+            .ReturnsAsync(new Employee());
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnDbVm()
+    {
+        var employee = new Employee
+        {
+            EmpNo = 451,
+            BirthDate = DateTime.Now,
+            FirstName = "TEST",
+            HireDate = DateTime.Now.AddDays(-4),
+            LastName = "WOW"
+        };
+        _fixture.DbConnection
+            .SetupDapperAsync(c => c.QueryFirstOrDefaultAsync<Employee>(It.IsAny<string>(), null, null, null, null))
+            .ReturnsAsync(employee);
+        var result = await _fixture.SendAsync(new GetEmployeeByIdQuery { EmployeeNumber = EmployeeNumber });
+        result.Should().NotBeNull();
+        result.Should().BeEquivalentTo(employee);
+    }
+}
