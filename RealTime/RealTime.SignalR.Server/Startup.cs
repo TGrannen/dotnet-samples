4339cfda3438c09fe79793e211a6f529f3bf7b54
Tim Grannen <grannendev@gmail.com>
2022-08-26 00:09:16 -0400

    Removing unused usings

--- RealTime/RealTime.SignalR.Server/Startup.cs
+++ RealTime/RealTime.SignalR.Server/Startup.cs
@@ -1,19 +1,10 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.ResponseCompression;
-using Microsoft.Identity.Web;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using RealTime.SignalR.Server.Hub;
 
