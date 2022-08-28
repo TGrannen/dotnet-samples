9ccf7df73bf5c766abe55983617af22f26634535
Tim Grannen <grannendev@gmail.com>
2022-08-27 23:57:52 -0400

    Add Initial Http client tests

---
+++ Testing/SnapShot/SnapshotTesting.Verify/Shared/HttpFixture.cs
@@ -0,0 +1,18 @@
+﻿namespace SnapshotTesting.Verify.Shared;
+
+public class HttpFixture : IDisposable
+{
+    public HttpFixture()
+    {
+        VerifyHttp.Enable();
+    }
+
+    public void Dispose()
+    {
+    }
+}
+
+[CollectionDefinition("Http Collection")]
+public class HttpCollection : ICollectionFixture<HttpFixture>
+{
+}
