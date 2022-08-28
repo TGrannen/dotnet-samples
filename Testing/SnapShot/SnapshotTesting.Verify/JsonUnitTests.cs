6228910918ce1c604e036df510e1be993d4301bf
Tim Grannen <grannendev@gmail.com>
2022-08-27 23:19:23 -0400

    Add Snapshot project and example tests

---
+++ Testing/SnapShot/SnapshotTesting.Verify/JsonUnitTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace SnapshotTesting.Verify;
+
+[UsesVerify]
+public class JsonUnitTests
+{
+    [Fact]
+    public Task VerifyJsonString()
+    {
+        var json = "{'key': {'msg': 'No action taken'}}";
+        return VerifyJson(json);
+    }
+
+    [Fact]
+    public Task VerifyJsonStream()
+    {
+        var json = "{'key': {'msg': 'No action taken'}}";
+        var stream = new MemoryStream(Encoding.UTF8.GetBytes(json));
+        return VerifyJson(stream);
+    }
+
+    [Fact]
+    public Task StreamMember()
+    {
+        var stream = new MemoryStream(Encoding.UTF8.GetBytes("value"));
+        return Verifier.Verify(new { stream });
+    }
+
+    [Fact]
+    public Task VerifyJsonJToken()
+    {
+        var json = "{'key': {'msg': 'No action taken'}}";
+        var target = JToken.Parse(json);
+        return Verifier.Verify(target);
+    }
+}
