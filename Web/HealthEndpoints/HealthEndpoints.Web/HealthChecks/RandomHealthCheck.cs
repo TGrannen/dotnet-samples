af7f2642e8352c05f758b495bc20731249796e58
Tim Grannen <grannendev@gmail.com>
2022-09-24 19:42:32 -0400

    Update Health Check samples to dotnet 6

--- Web/HealthEndpoints/HealthEndpoints.Web/HealthChecks/RandomHealthCheck.cs
+++ Web/HealthEndpoints/HealthEndpoints.Web/HealthChecks/RandomHealthCheck.cs
@@ -1,44 +1,35 @@
 using Bogus;
-using Bogus.Extensions;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Threading;
-using System.Threading.Tasks;
 
-namespace HealthEndpoints.Web.HealthChecks
+namespace HealthEndpoints.Web.HealthChecks;
+
+public class RandomHealthCheck : IHealthCheck
 {
-    public class RandomHealthCheck : IHealthCheck
+    private readonly ILogger<RandomHealthCheck> _logger;
+    private readonly Faker _faker;
+
+    public RandomHealthCheck(ILogger<RandomHealthCheck> logger)
     {
-        private readonly ILogger<RandomHealthCheck> _logger;
+        _logger = logger;
+        _faker = new Faker();
+    }
 
-        public RandomHealthCheck(ILogger<RandomHealthCheck> logger)
+    public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new())
+    {
+        try
         {
-            _logger = logger;
+            var result = _faker.Random.Number(0, 5).OrNull(_faker, 0.2f);
+            return result switch
+            {
+                null => throw new Exception("Bad Response"),
+                > 4 => Task.FromResult(HealthCheckResult.Degraded("Values are not looking good")),
+                _ => Task.FromResult(HealthCheckResult.Healthy())
+            };
         }
-
-        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
+        catch (Exception exception)
         {
-            try
-            {
-                var faker = new Faker();
-                var result = faker.Random.Number(0, 5).OrNull(faker, 0.2f);
-                if (result == null)
-                {
-                    throw new Exception("Bad Response");
-                }
-
-                if (result > 4)
-                {
-                    return Task.FromResult(HealthCheckResult.Degraded("Values are not looking good"));
-                }
-                return Task.FromResult(HealthCheckResult.Healthy());
-            }
-            catch (Exception exception)
-            {
-                _logger.LogWarning("Error during check: {Message}", exception.Message);
-                return Task.FromResult(HealthCheckResult.Unhealthy(exception.Message));
-            }
+            _logger.LogWarning("Error during check: {Message}", exception.Message);
+            return Task.FromResult(HealthCheckResult.Unhealthy(exception.Message));
         }
     }
 }
--- Web/HealthEndpoints/HealthEndpoints.Web/Program.cs
+++ Web/HealthEndpoints/HealthEndpoints.Web/Program.cs
@@ -1,20 +1,52 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using HealthChecks.UI.Client;
+using HealthEndpoints.Web.HealthChecks;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Serilog;
 
-namespace HealthEndpoints.Web
+var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddHealthChecks()
+    .AddCheck<RandomHealthCheck>("Random", null, new[] { "Dummy" })
+    .AddCheck<RandomHealthCheck>("Random2", null, new[] { "Dummy" })
+    .AddCheck<RandomHealthCheck>("Random3", null, new[] { "Dummy" })
+    .AddCheck("Inline", () => HealthCheckResult.Degraded("I'm not sure what's going on here"), new[] { "sql" })
+    ;
+
+builder.Services
+    .AddHealthChecksUI(setup => setup.DisableDatabaseMigrations())
+    .AddInMemoryStorage();
+
+builder.Host.UseSerilog((context, configuration) => { configuration.ReadFrom.Configuration(context.Configuration); });
+
+var app = builder.Build();
+app.UseSerilogRequestLogging();
+
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    app.UseDeveloperExceptionPage();
 }
+else
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapHealthChecksUI();
+    endpoints.MapHealthChecks("/health", new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
+    endpoints.MapHealthChecks("/health/sql", new HealthCheckOptions
+    {
+        Predicate = c => c.Tags.Contains("sql"),
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
+});
+
+await app.RunAsync();
