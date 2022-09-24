5a31db7f403995d492d7800ffc7731af981e37fc
Tim Grannen <grannendev@gmail.com>
2022-09-24 00:25:58 -0400

    Update NUnit samples to dotnet 6

--- Testing/NUnit/NUnit.Tests/CalcServiceTestsWithFixture.cs
+++ Testing/NUnit/NUnit.Tests/CalcServiceTestsWithFixture.cs
@@ -1,55 +1,53 @@
 using NUnit.BusinessLogic.Services;
-using NUnit.Framework;
 
-namespace NUnit.Tests
+namespace NUnit.Tests;
+
+[TestFixture(50, 100.00)]
+[TestFixture(-50, 20)]
+public class CalcServiceTestsWithFixture
 {
-    [TestFixture(50, 100.00)]
-    [TestFixture(-50, 20)]
-    public class CalcServiceTestsWithFixture
+    private readonly double _first;
+    private readonly double _second;
+
+    public CalcServiceTestsWithFixture(double first, double second)
     {
-        private readonly double _first;
-        private readonly double _second;
+        _first = first;
+        _second = second;
+    }
 
-        public CalcServiceTestsWithFixture(double first, double second)
-        {
-            _first = first;
-            _second = second;
-        }
+    private CalcService _calcService;
 
-        private CalcService _calcService;
+    [SetUp]
+    public void Setup()
+    {
+        _calcService = new CalcService();
+    }
 
-        [SetUp]
-        public void Setup()
-        {
-            _calcService = new CalcService();
-        }
+    [TestCase]
+    public void AddMethod()
+    {
+        var result = _calcService.PerformOperation(_first, _second, "a");
+        Assert.AreEqual(_first + _second, result);
+    }
 
-        [TestCase]
-        public void AddMethod()
-        {
-            var result = _calcService.PerformOperation(_first, _second, "a");
-            Assert.AreEqual(_first + _second, result);
-        }
+    [TestCase]
+    public void SubtractMethod()
+    {
+        var result = _calcService.PerformOperation(_first, _second, "s");
+        Assert.AreEqual(_first - _second, result);
+    }
 
-        [TestCase]
-        public void SubtractMethod()
-        {
-            var result = _calcService.PerformOperation(_first, _second, "s");
-            Assert.AreEqual(_first - _second, result);
-        }
+    [TestCase]
+    public void MultiplyMethod()
+    {
+        var result = _calcService.PerformOperation(_first, _second, "m");
+        Assert.AreEqual(_first * _second, result);
+    }
 
-        [TestCase]
-        public void MultiplyMethod()
-        {
-            var result = _calcService.PerformOperation(_first, _second, "m");
-            Assert.AreEqual(_first * _second, result);
-        }
-
-        [TestCase]
-        public void DivideMethod()
-        {
-            var result = _calcService.PerformOperation(_first, _second, "d");
-            Assert.AreEqual(_first / _second, result);
-        }
+    [TestCase]
+    public void DivideMethod()
+    {
+        var result = _calcService.PerformOperation(_first, _second, "d");
+        Assert.AreEqual(_first / _second, result);
     }
 }
---
+++ Testing/NUnit/NUnit.Tests/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿// Global using directives
+
+global using System.Collections.Generic;
+global using System.Linq;
+global using NUnit.Framework;
