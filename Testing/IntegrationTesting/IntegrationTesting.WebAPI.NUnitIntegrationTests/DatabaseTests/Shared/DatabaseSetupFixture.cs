ba48c0022a116651a37b3d83cc01b0bb986ef45c
Tim Grannen <grannendev@gmail.com>
2022-10-17 21:37:12 -0400

    Add NUnit Integration test examples

---
+++ Testing/IntegrationTesting/IntegrationTesting.WebAPI.NUnitIntegrationTests/DatabaseTests/Shared/DatabaseSetupFixture.cs
@@ -0,0 +1,32 @@
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Configurations;
+using DotNet.Testcontainers.Containers;
+
+namespace IntegrationTesting.WebAPI.NUnitIntegrationTests.DatabaseTests;
+
+[SetUpFixture]
+public class DatabaseSetupFixture
+{
+    private readonly PostgreSqlTestcontainer _container = new TestcontainersBuilder<PostgreSqlTestcontainer>()
+        .WithDatabase(new PostgreSqlTestcontainerConfiguration
+        {
+            Database = "integrationTestDb",
+            Username = "postgres",
+            Password = "test-password",
+        }).Build();
+
+    [OneTimeSetUp]
+    public async Task OneTimeSetUp()
+    {
+        await _container.StartAsync();
+        await DbSeeder.CreateSchema(_container.ConnectionString);
+        await DbSeeder.SeedData(_container.ConnectionString);
+        DatabaseSetupFixtureTestData.ConnectionString = _container.ConnectionString;
+    }
+
+    [OneTimeTearDown]
+    public async Task OneTimeTearDown()
+    {
+        await _container?.StopAsync()!;
+    }
+}
---
+++ Testing/IntegrationTesting/IntegrationTesting.WebAPI.NUnitIntegrationTests/Shared/Extensions.cs
@@ -0,0 +1,9 @@
+﻿namespace IntegrationTesting.WebAPI.NUnitIntegrationTests.Shared;
+
+public static class Extensions
+{
+    public static T CreateRefitClient<TFactory, T>(this WebApplicationFactory<TFactory> factory) where TFactory : class
+    {
+        return RestService.For<T>(factory.CreateClient());
+    }
+}
