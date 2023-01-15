058d9fe6f8c46dbe07b4a84fe166fab574cccd30
Tim Grannen <grannendev@gmail.com>
2023-01-15 12:20:52 -0500

    Set up Serilog

--- Web/Blazor/BlazorClient.AuthExample/Program.cs
+++ Web/Blazor/BlazorClient.AuthExample/Program.cs
@@ -2,10 +2,18 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorClient.AuthExample;
 using MudBlazor.Services;
+using Serilog;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+builder.Logging.SetMinimumLevel(LogLevel.None);
+builder.Logging.AddSerilog();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
