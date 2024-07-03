462a0e42f70fb9b3d63f1d2b2add144f90623dc2
Tim Grannen <grannendev@gmail.com>
2024-07-02 21:19:45 -0400

    Add controllers to Ocelot example

---
+++ ReverseProxy/ReverseProxy.OcelotDemo/Controllers/ValuesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReverseProxy.OcelotDemo.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ValuesController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new string[] { "value1", "value2" });
+    }
+}
--- ReverseProxy/ReverseProxy.OcelotDemo/Program.cs
+++ ReverseProxy/ReverseProxy.OcelotDemo/Program.cs
@@ -5,11 +5,17 @@ using Serilog;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseSerilog((context, configuration) => { configuration.ReadFrom.Configuration(context.Configuration); });
+builder.Services.AddControllers();
 
 builder.Configuration.AddJsonFile("ocelot.json");
 builder.Services.AddOcelot();
 
 var app = builder.Build();
 app.UseHttpsRedirection();
+app.UseRouting();
+
+// Must use UseEndpoints here or it will not work 🤷‍♂️
+app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
 app.UseOcelot().Wait();
 app.Run();
