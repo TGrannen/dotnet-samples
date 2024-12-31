6be79d52b970a408efa638402f77a4aaa1498202
Tim Grannen <grannendev@gmail.com>
2024-12-30 20:25:31 -0500

    Add enhanced Azure App Config setup

--- Configuration/AzureAppConfigSample/Program.cs
+++ Configuration/AzureAppConfigSample/Program.cs
@@ -1,4 +1,6 @@
 using Azure.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 using Microsoft.Extensions.Options;
 using Microsoft.FeatureManagement;
 using Serilog;
@@ -20,6 +22,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.Configure<Settings>(builder.Configuration.GetSection("TestSettings"));
 var connectionString = builder.Configuration.GetConnectionString("AppConfig");
+var serviceName = builder.Configuration.GetValue("AppConfig:ServiceName", "Not-Set");
 
 builder.Services.AddFeatureManagement();
 builder.Services.AddAzureAppConfiguration();
@@ -27,12 +30,13 @@ builder.Configuration.AddAzureAppConfiguration(options =>
 {
     options.Connect(connectionString)
         .Select("*")
+        .Select("*", serviceName)
         .ConfigureRefresh(refreshOptions =>
         {
-            refreshOptions.Register("AppConfig:Sentinel", refreshAll: true);
+            refreshOptions.Register("AppConfig:Sentinel", refreshAll: true).SetRefreshInterval(TimeSpan.FromSeconds(15));
         });
     options.ConfigureKeyVault(keyVaultOptions => { keyVaultOptions.SetCredential(new DefaultAzureCredential()); });
-    options.UseFeatureFlags();
+    options.UseFeatureFlags(flagOptions => flagOptions.SetRefreshInterval(TimeSpan.FromSeconds(15)));
 });
 
 var app = builder.Build();
@@ -50,18 +54,35 @@ changeToken.RegisterChangeCallback(state => { Log.Information("Configuration has
 app.UseSerilogRequestLogging();
 app.UseHttpsRedirection();
 app.UseAzureAppConfiguration();
-app.MapGet("/Settings",
-    async (IConfiguration configuration, IFeatureManager featureManager, IOptions<Settings> options, IOptionsSnapshot<Settings> optionsSnapshot) =>
-        Results.Ok(new
+
+app.MapGet("/Settings", async (IConfiguration configuration,
+        IFeatureManager featureManager,
+        IOptions<Settings> options,
+        IOptionsSnapshot<Settings> optionsSnapshot,
+        IOptionsMonitor<Settings> optionsMonitor) =>
+    Results.Ok(new
+    {
+        Settings1 = new
         {
-            Settings1 = new
-            {
-                Options = options.Value,
-                Snapshot = optionsSnapshot.Value
-            },
-            RemoteValue = configuration.GetValue<string>("ConnectionStrings:MyConnectionString"),
-            FeatureFlag = await featureManager.IsEnabledAsync("TestFeature")
-        }));
+            Options = options.Value,
+            Snapshot = optionsSnapshot.Value,
+            Monitor = optionsMonitor.CurrentValue
+        },
+        RemoteValue = configuration.GetValue<string>("ConnectionStrings:MyConnectionString"),
+        FeatureFlag = await featureManager.IsEnabledAsync("TestFeature")
+    }));
+
+app.MapPost("/Refresh", async ([FromServices] IConfigurationRefresherProvider configurationRefresher) =>
+{
+    var result = new List<bool>();
+    foreach (var refresher in configurationRefresher.Refreshers)
+    {
+        var success = await refresher.TryRefreshAsync();
+        result.Add(success);
+    }
+
+    return Results.Ok(result);
+});
 
 app.Run();
 
