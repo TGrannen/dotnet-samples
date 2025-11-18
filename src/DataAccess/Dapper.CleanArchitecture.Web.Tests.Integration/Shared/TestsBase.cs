f25837cb767a2954a33b72d57da5a4a8de1eaa74
Tim Grannen <grannendev@gmail.com>
2025-11-17 19:16:35 -0500

    Upgrade DataAccess to net9.0

--- src/DataAccess/Dapper.CleanArchitecture.Web.Tests.Integration/Shared/TestsBase.cs
+++ src/DataAccess/Dapper.CleanArchitecture.Web.Tests.Integration/Shared/TestsBase.cs
@@ -5,28 +5,42 @@ using Respawn;
 
 namespace Dapper.CleanArchitecture.Web.Tests.Integration.Shared;
 
-public abstract class TestsBase : IDisposable
+public abstract class TestsBase : IAsyncDisposable
 {
     private readonly AppFactory _factory;
-    private static Checkpoint _checkpoint = null!;
+    private readonly Respawner _respawner;
 
     protected TestsBase(AppFactory factory)
     {
         _factory = factory;
-        _checkpoint ??= new Checkpoint
-        {
-            DbAdapter = DbAdapter.Postgres
-        };
-    }
-
-    public void Dispose()
-    {
-        var logger =_factory.Services.GetRequiredService<ILogger<TestsBase>>();
-        var connectionStringProvider =_factory.Services.GetRequiredService(typeof(IDbConnectionStringProvider)) as IDbConnectionStringProvider;
-        var connectionString = connectionStringProvider!.GetConnectionString();
+        var connectionString = ConnectionString();
         using var conn = new NpgsqlConnection(connectionString);
         conn.Open();
+        _respawner = Respawner.CreateAsync(conn, new RespawnerOptions
+        {
+            SchemasToInclude =
+            [
+                "public"
+            ],
+            DbAdapter = DbAdapter.Postgres
+        }).GetAwaiter().GetResult();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        var logger = _factory.Services.GetRequiredService<ILogger<TestsBase>>();
+        var connectionString = ConnectionString();
+        await using var conn = new NpgsqlConnection(connectionString);
+        await conn.OpenAsync();
+        await _respawner.ResetAsync(conn);
         logger.LogInformation("Wiping Database");
-        _checkpoint.Reset(conn).Wait();
+        await _factory.DisposeAsync();
+    }
+
+    private string ConnectionString()
+    {
+        var connectionStringProvider = _factory.Services.GetRequiredService(typeof(IDbConnectionStringProvider)) as IDbConnectionStringProvider;
+        var connectionString = connectionStringProvider!.GetConnectionString();
+        return connectionString;
     }
 }
