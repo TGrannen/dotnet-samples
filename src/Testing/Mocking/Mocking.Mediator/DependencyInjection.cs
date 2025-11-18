c2261a944f7ee3d376ea525ac2049a243983292a
Tim Grannen <grannendev@gmail.com>
2025-11-17 19:37:11 -0500

    Upgrade Mocking projects to .NET 10 and update package dependencies

--- src/Testing/Mocking/Mocking.Mediator/DependencyInjection.cs
+++ src/Testing/Mocking/Mocking.Mediator/DependencyInjection.cs
@@ -6,7 +6,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddMediatR(typeof(DependencyInjection));
+        services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
 
         return services;
     }
