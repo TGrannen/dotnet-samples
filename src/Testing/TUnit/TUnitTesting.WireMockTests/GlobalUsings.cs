0de3dc1236c04c93c900ec997956b041a1755066
Tim Grannen <grannendev@gmail.com>
2026-04-17 21:19:52 -0400

    Add WireMock testing project

---
+++ src/Testing/TUnit/TUnitTesting.WireMockTests/GlobalUsings.cs
@@ -0,0 +1,9 @@
+// Global using directives
+
+global using System.Net;
+global using Microsoft.AspNetCore.Mvc.Testing;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.DependencyInjection;
+global using TUnit.AspNetCore;
+global using TUnit.Core.Extensions;
+global using TUnit.Core.Interfaces;
